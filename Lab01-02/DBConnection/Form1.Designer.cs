7b3c0494be49b5fe32547c14dd1c818686b2979f
SkyShadowMeadow <skyshadowmeadow.91@gmail.com>
2020-07-20 10:15:42 +0300

    Добавлены лабораторные работы 1-6

---
+++ Lab01-02/DBConnection/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.работаСБазойДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подключитьсяКБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отключитьсяОтБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ассинхронноеПодключениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокПодключенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.работаСБазойДанныхToolStripMenuItem,
+            this.списокПодключенийToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(335, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // работаСБазойДанныхToolStripMenuItem
+            // 
+            this.работаСБазойДанныхToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.подключитьсяКБДToolStripMenuItem,
+            this.отключитьсяОтБДToolStripMenuItem,
+            this.ассинхронноеПодключениеToolStripMenuItem});
+            this.работаСБазойДанныхToolStripMenuItem.Name = "работаСБазойДанныхToolStripMenuItem";
+            this.работаСБазойДанныхToolStripMenuItem.Size = new System.Drawing.Size(144, 20);
+            this.работаСБазойДанныхToolStripMenuItem.Text = "Работа с базой данных";
+            // 
+            // подключитьсяКБДToolStripMenuItem
+            // 
+            this.подключитьсяКБДToolStripMenuItem.Name = "подключитьсяКБДToolStripMenuItem";
+            this.подключитьсяКБДToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.подключитьсяКБДToolStripMenuItem.Text = "Подключиться к БД";
+            this.подключитьсяКБДToolStripMenuItem.Click += new System.EventHandler(this.подключитьсяКБДToolStripMenuItem_Click);
+            // 
+            // отключитьсяОтБДToolStripMenuItem
+            // 
+            this.отключитьсяОтБДToolStripMenuItem.Name = "отключитьсяОтБДToolStripMenuItem";
+            this.отключитьсяОтБДToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.отключитьсяОтБДToolStripMenuItem.Text = "Отключиться от БД";
+            this.отключитьсяОтБДToolStripMenuItem.Click += new System.EventHandler(this.отключитьсяОтБДToolStripMenuItem_Click);
+            // 
+            // ассинхронноеПодключениеToolStripMenuItem
+            // 
+            this.ассинхронноеПодключениеToolStripMenuItem.Name = "ассинхронноеПодключениеToolStripMenuItem";
+            this.ассинхронноеПодключениеToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.ассинхронноеПодключениеToolStripMenuItem.Text = "Ассинхронное подключение";
+            this.ассинхронноеПодключениеToolStripMenuItem.Click += new System.EventHandler(this.ассинхронноеПодключениеToolStripMenuItem_Click);
+            // 
+            // списокПодключенийToolStripMenuItem
+            // 
+            this.списокПодключенийToolStripMenuItem.Name = "списокПодключенийToolStripMenuItem";
+            this.списокПодключенийToolStripMenuItem.Size = new System.Drawing.Size(140, 20);
+            this.списокПодключенийToolStripMenuItem.Text = "Список подключений";
+            this.списокПодключенийToolStripMenuItem.Click += new System.EventHandler(this.списокПодключенийToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сколько продуктов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сколько продуктов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Посчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 139);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(305, 214);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Список продуктов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 112;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Цена";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Фасовка";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 420);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Транзакция";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 560);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem работаСБазойДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подключитьсяКБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отключитьсяОтБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ассинхронноеПодключениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокПодключенийToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab04/CreatingDataTable/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CreatingDataTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private DataSet dataSet;
+        private BindingSource bindingSource1;
+        private BindingSource bindingSource2;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void MakeDataTables() 
+        {
+            MakeParentTable(); 
+            MakeChildTable();
+            MakeDataRelation(); 
+            BindToDataGrid(); 
+        }
+
+        private void MakeParentTable()
+        {
+            DataTable table = new DataTable("ParentTable");
+            DataColumn column; DataRow row;
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "id"; 
+            column.ReadOnly = true; 
+            column.Unique = true;
+            table.Columns.Add(column);
+
+            column = new DataColumn(); 
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ParentItem";
+            column.AutoIncrement = false; 
+            column.Caption = "ParentItem"; 
+            column.ReadOnly = false; 
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            table.Columns.Add("Total", typeof(Double)); 
+            table.Columns.Add("SalesTax", typeof(Double), "Total * 0.13");
+
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1]; 
+            PrimaryKeyColumns[0] = table.Columns["id"]; 
+            table.PrimaryKey = PrimaryKeyColumns;
+
+            dataSet = new DataSet(); 
+            dataSet.Tables.Add(table);
+
+            for (int i = 0; i <= 2; i++) 
+            {
+                row = table.NewRow(); 
+                row["id"] = i; row["ParentItem"] = "ParentItem " + i; row["Total"] = 2 * i + 0.5; 
+                table.Rows.Add(row); 
+            }
+        }
+
+        private void MakeChildTable()
+        {
+            DataTable table = new DataTable("childTable");
+            DataColumn column; DataRow row;
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32"); 
+            column.ColumnName = "ChildID"; 
+            column.AutoIncrement = true; 
+            column.Caption = "ID"; 
+            column.ReadOnly = true; 
+            column.Unique = true;
+            table.Columns.Add(column);
+            
+            column = new DataColumn(); 
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ChildItem"; 
+            column.AutoIncrement = false; 
+            column.Caption = "ChildItem"; 
+            column.ReadOnly = false; 
+            column.Unique = false; 
+            table.Columns.Add(column);
+            
+            column = new DataColumn(); 
+            column.DataType = System.Type.GetType("System.Int32"); 
+            column.ColumnName = "ParentID"; column.AutoIncrement = false; 
+            column.Caption = "ParentID"; 
+            column.ReadOnly = false; 
+            column.Unique = false; 
+            table.Columns.Add(column);
+            
+            dataSet.Tables.Add(table);
+            for (int i = 0; i <= 4; i++) 
+            { 
+                row = table.NewRow(); 
+                row["childID"] = i; 
+                row["ChildItem"] = "Item " + i;
+                row["ParentID"] = 0; 
+                table.Rows.Add(row); 
+            }
+            for (int i = 0; i <= 4; i++) 
+            { 
+                row = table.NewRow(); 
+                row["childID"] = i + 5; 
+                row["ChildItem"] = "Item " + i; 
+                row["ParentID"] = 1; 
+                table.Rows.Add(row);
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow(); row["childID"] = i + 10;
+                row["ChildItem"] = "Item " + i; row["ParentID"] = 2; 
+                table.Rows.Add(row);
+            }
+        }
+        private void MakeDataRelation()
+        { // DataRelation requires two DataColumn 
+          // (parent and child) and a name. 
+            DataColumn parentColumn = dataSet.Tables["ParentTable"].Columns["id"]; 
+            DataColumn childColumn = dataSet.Tables["ChildTable"].Columns["ParentID"];
+            DataRelation relation = new DataRelation("parent2Child", parentColumn, childColumn); 
+            dataSet.Tables["ChildTable"].ParentRelations.Add(relation);
+        }
+
+        private void BindToDataGrid()
+        {
+            bindingSource1 = new BindingSource(); 
+            bindingSource2 = new BindingSource();
+            bindingSource1.DataSource = dataSet.Tables["ParentTable"]; 
+            bindingSource2.DataSource = dataSet.Tables["childTable"];
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Заполнить дочернюю таблицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заполнить основную таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 288);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(542, 150);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private DataGridView dataGridView2;
+    }
+}
+
